6357282de1341fb1eea46e730d85c463a1aa0fb4
DHL <david.hernandez@uclm.es>
2025-10-16 18:00:35 +0200

    .

--- CompoundProjectedCRSDialog.py
+++ CompoundProjectedCRSDialog.py
@@ -7,7 +7,7 @@ import math
 import json
 
 
-current_path = os.path.dirname(__file__)
+current_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(current_path, '..'))
 
 from .CRSsTools import CRSsTools
